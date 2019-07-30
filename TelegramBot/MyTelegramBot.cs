555944e2e870828aeee8ccca32bb3636adb25a9a
arslan174 <arslan.ishemgulov@gmail.com>
2019-07-30 17:01:40 +0500

    change to console app

---
+++ TelegramBot/MyTelegramBot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot
+{
+    static class MyTelegramBot
+    {
+        private static string Token = "963348734:AAGb_uFqxSh0hi3ajgEYCWEelxwvU2nnlUE";
+        static public TelegramBotClient MyBotClient;
+        private static string[] FilmsArray = {
+            "Начало", "Остров проклятых", "Бойцовский клуб", "Однажды в Америке",
+        "Гран Торино", "Бесславные ублюдки", "Интерстеллар", "Отступники"};
+
+        public static void InitializeMyBot()
+        {
+            WebProxy wp = new WebProxy("185.146.112.24:8080", true);
+            MyBotClient = new TelegramBotClient(Token, wp);
+            MyBotClient.OnMessage += MessageHandler;
+            MyBotClient.StartReceiving();
+        }
+
+        static void MessageHandler(object sender, MessageEventArgs messageEventArgs)
+        {
+            string replyMessage = null;
+            
+
+            if (messageEventArgs.Message.Text.Trim(' ').ToLower() == "посоветуй фильм")
+            {
+                var rnd = new Random();
+                replyMessage = FilmsArray[rnd.Next(FilmsArray.Length)];
+            }
+
+            if (messageEventArgs.Message.Text.Trim(' ').ToLower() == "/start")
+            {
+                replyMessage = "Привет!";
+
+            }
+
+            var markup = new ReplyKeyboardMarkup(new KeyboardButton("Посоветуй фильм"));
+            markup.OneTimeKeyboard = false;
+            
+            var chatId = messageEventArgs.Message.Chat.Id;
+            ((TelegramBotClient)sender).SendTextMessageAsync(chatId, replyMessage, replyMarkup: markup);
+        }
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Args;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Net;
+using System.Threading;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        public static ManualResetEvent waitHandle = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            MyTelegramBot.InitializeMyBot();
+            var me = MyTelegramBot.MyBotClient.GetMeAsync().Result;
+            Console.WriteLine(me);
+
+            waitHandle.WaitOne();
+        }
+    }
+}
